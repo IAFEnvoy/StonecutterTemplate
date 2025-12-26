bee8e41f45f7305067d55efcd27d45636c581c92
IAFEnvoy <tanksheng545@126.com>
2025-12-26 18:33:40 +0800

    add new choice

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,14 +3,14 @@ plugins {
     id("co.uzzu.dotenv.gradle") version "4.0.0"
     id("fabric-loom") version "1.11-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.115" apply false
-    id ("dev.kikugie.postprocess.jsonlang") version "2.1-beta.4" apply false
+    id("dev.kikugie.postprocess.jsonlang") version "2.1-beta.4" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false
 }
 
 stonecutter active "1.21.10-fabric"
 
 stonecutter parameters {
-    constants.match(node.metadata.project.substringAfterLast('-'), "fabric", "neoforge")
+    constants.match(node.metadata.project.substringAfterLast('-'), "fabric", "neoforge", "forge")
     filters.include("**/*.fsh", "**/*.vsh")
 }
 
